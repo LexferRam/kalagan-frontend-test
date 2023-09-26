f73ee2a8e9c75cf37ad6de7c06afb0f7a687413c
Lexfer Ramirez <lexferramirez@gmail.com>
2023-09-26 02:42:43 -0400

    [ADD] navbar to layout

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from 'next/link';
+
+function ResponsiveAppBar() {
+
+    return (
+        <AppBar position="static" style={{ backgroundColor: 'white', position: 'fixed', zIndex: 10 }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Link href="/" style={{ display: 'flex', alignItems: 'center' }}>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            color="primary"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            Podcaster
+                        </Typography>
+                    </Link>
+
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'blue',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default ResponsiveAppBar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import ResponsiveAppBar from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ResponsiveAppBar />
+        <div style={{ marginTop: 75 }}>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
